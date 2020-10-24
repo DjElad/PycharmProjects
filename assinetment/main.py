bf1a606e8bac731448b91426904907b4c49388d2
DjElad <71558318+DjElad@users.noreply.github.com>
2020-10-24 11:42:12 +0300

    phase 2 done

--- assinetment/main.py
+++ assinetment/main.py
@@ -1,5 +1,6 @@
 import json
-import uuid
+from os import listdir
+from os.path import isfile, join
 from flask import Flask, Response, redirect, url_for, request, jsonify, render_template
 from hello import Todo
 
@@ -12,7 +13,21 @@ def create_file():
     chore = Todo(user_input["desc"], user_input["severity"])
     with open(fr"C:\Users\Amir\PycharmProjects\assinetment\{chore.get_uuid()}.json", "w") as f:
         json.dump(chore.to_json(), f)
-    return jsonify(chore.to_json())
+
+    return jsonify(chore.to_json()), 201
+
+
+@app.route('/todos', methods=['GET'])
+def all_todos():
+    path = r"C:\Users\Amir\PycharmProjects\assinetment"
+    only_files = [f for f in listdir(path) if isfile(join(path, f))]
+    output = []
+    for f in only_files:
+        if f.endswith(".json"):
+            with open(fr"{path}\{f}") as j:
+                content = json.load(j)
+                output.append(content)
+    return jsonify(Todos=output), 201
 
 
 if __name__ == '__main__':
