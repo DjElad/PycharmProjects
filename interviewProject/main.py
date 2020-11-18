cb9c39c56547c9f0f2a0352b6db5f86843264a58
DjElad <71558318+DjElad@users.noreply.github.com>
2020-11-18 12:12:02 +0200

    init

--- interviewProject/main.py
+++ interviewProject/main.py
@@ -1,4 +1,10 @@
 import pytest
 import json
-import flask
+from flask import Flask, Response, redirect, url_for, request, jsonify, render_template
 
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
